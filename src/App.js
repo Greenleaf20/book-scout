0568e8f75e26b97e1f2c3260ac7af8437fe05f53
Greenleaf20 <archanaganes@umass.edu>
2024-07-22 02:03:15 -0400

    Add input validation and modify css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Card } from 'flowbite-react';
 import './App.css';
 import InputForm from './components/InputForm/InputForm';
-import bg from './images/bg-crop.webp';
+import bg from './images/bg-crop2.webp';
+// import bg1 from './images/bg.webp';
 
 function App() {
   return (
     <div className="App">
-      <Card className="max-w-sm input-card" imgAlt="Book shelf" imgSrc={bg} style={{ minWidth: '100vw', height: '55vh'}}>
+      <Card className="max-w-sm input-card" imgAlt="Book shelf" imgSrc={bg} style={{ minWidth: '99vw', height: '97vh'}}>
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-center ">
               Book Scout
           </h5>
--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,18 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, TextInput } from "flowbite-react";
 import './InputForm.css';
+import { useState } from "react";
+import { HiInformationCircle } from "react-icons/hi";
 
 function InputForm() {
+    const [keyword, setKeyword] = useState('');
+    const [emptyAlert, setEmptyAlert] = useState(false);
+
+    const searchBooks = (event) => {
+        event.preventDefault();
+        if (keyword === '') {
+            setEmptyAlert(true);
+            setTimeout(() => {
+                setEmptyAlert(false);
+            },5000);
+            return;
+        }
+
+        console.log("Searching for books", keyword);
+    }
+
+    const handleTextChange = (event) => {
+        setKeyword(event.target.value);
+    }
+
     return (
-        <div className="input-container flex justify-center items-start">
-           <form className="flex max-w-md flex-col gap-4">
-                <div>
-                    <div className="mb-2 block input-container">
-                        <Label htmlFor="small" value="Enter keyword here" className="input-label"/>
+        <div className="input-container">
+            <div className="flex justify-center items-start">
+                <form className="flex max-w-md flex-col gap-4">
+                    <div>
+                        <div className="mb-2 block input-container">
+                            <Label htmlFor="small" value="Enter keyword here" className="input-label"/>
+                        </div>
+                        <TextInput id="small" type="text" sizing="sm" onChange={handleTextChange}/>
                     </div>
-                    <TextInput id="small" type="text" sizing="sm" />
-                </div>
-                <Button gradientDuoTone="purpleToPink" type="submit">Search</Button>
-            </form>
+                    <Button gradientDuoTone="purpleToPink" type="submit" onClick={searchBooks}>Search</Button>
+                </form>
+            </div>
+            <div className='input-alert-container flex justify-center items-center mt-4'>
+                {
+                    emptyAlert && <Alert color="failure" icon={HiInformationCircle} className="max-w-md w-full">
+                        <span className="font-medium input-alert">Empty input!</span> Enter keyword to search for books.
+                    </Alert>
+                }
+            </div>
         </div>
     );
 }
